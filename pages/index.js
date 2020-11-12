de5a5a05ce7d427d14ee510bb07f103c62cc2493
Alexander Grattan <alex@grattan.me>
2020-11-12 16:27:07 -0500

    Broken ScrollTrigger

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,37 @@
 import Head from "next/head";
 import Link from "next/link";
-import Image from "next/image";
-import gsap from "gsap";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    let projects = gsap.utils.toArray(".project");
+
+    gsap.to(projects, {
+      xPercent: -100 * (projects.length - 1),
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".project-row",
+        pin: true,
+        start: "top top",
+        scrub: true,
+        snap: {
+          snapTo: 1 / (projects.length - 1),
+          duration: { min: 0.2, max: 0.3 },
+          delay: 0,
+        },
+        // Base vertical scrolling on how wide the container is so it feels more natural.
+        end: () =>
+          "+=" + document.querySelector(".project-row").offsetHeight,
+      },
+    });
+
     gsap.to("body", { duration: 0, css: { visibility: "visible" } });
     gsap.from(".title", { duration: 1, y: 20, opacity: 0, ease: "power4.out" });
   }, []);
