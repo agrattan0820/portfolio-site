de0ab5ade4bb13efedfc4bd2166840ed842301c7
Alexander Grattan <alex@grattan.me>
2020-11-13 13:30:45 -0500

    Ball intro animation

---
+++ components/introOverlay.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function IntroOverlay() {
+  return (
+    <div className="intro-overlay">
+      <div className="ball"></div>
+    </div>
+  );
+}
+
+export default IntroOverlay;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Head from "next/head";
 import Link from "next/link";
+import IntroOverlay from "../components/introOverlay";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -43,6 +44,14 @@ export default function Home() {
   ];
 
   const [dimensions, setDimensions] = useState();
+  const [animationComplete, setAnimationComplete] = useState(false);
+  const projectsRef = useRef(null);
+
+  const completeAnimation = () => {
+    setAnimationComplete(true);
+  };
+
+  const executeScroll = () => projectsRef.current.scrollIntoView();
 
   useEffect(() => {
     let vh = window.innerHeight * 0.01;
@@ -74,56 +83,77 @@ export default function Home() {
 
     window.addEventListener("resize", debouncedHandleResize);
 
-    let tl = gsap.timeline();
-
     gsap.registerPlugin(ScrollTrigger);
 
-    let projects = gsap.utils.toArray(".project-row .project");
+    const homeAnimation = (animation) => {
+      let tl = gsap.timeline();
 
-    tl.to("body", { duration: 0, css: { visibility: "visible" } })
-      .from(".title", {
-        duration: 0.8,
-        y: 100,
+      let projects = gsap.utils.toArray(".project-row .project");
+
+      tl.to("body", { duration: 0, css: { visibility: "visible" } })
+        .from(".ball", {
+          duration: 2,
+          y: "-100vh",
+          ease: "bounce.out",
+        })
+        .to(".ball", {
+          duration: 1,
+          scale: 25,
+          ease: "power4.out",
+          onComplete: animation,
+        })
+        .from(".after-animation", {
+          duration: 0.8,
+          opacity: 0,
+          ease: "power4.out",
+        })
+        .from(".title", {
+          duration: 0.8,
+          y: 100,
+          opacity: 0,
+          delay: 0.2,
+          ease: "power4.out",
+        })
+        .from(".job-title", {
+          duration: 0.8,
+          y: 100,
+          opacity: 0,
+          ease: "power4.out",
+        });
+
+      gsap.from(".project-row", {
+        scrollTrigger: {
+          trigger: ".project-container",
+          start: "top bottom",
+          toggleActions: "restart",
+        },
+        x: "100vw",
         opacity: 0,
-        delay: 0.2,
-        ease: "power4.out",
-      })
-      .from(".job-title", {
-        duration: 0.8,
-        y: 100,
-        opacity: 0,
+        duration: 2,
         ease: "power4.out",
       });
 
-    gsap.from(".project-row", {
-      scrollTrigger: {
-        trigger: ".project-container",
-        start: "top bottom",
-        toggleActions: "restart",
-      },
-      x: "100vw",
-      opacity: 0,
-      duration: 2,
-      ease: "power4.out",
-    });
-
-    gsap.to(projects, {
-      xPercent: -100 * (projects.length - 1),
-      ease: "none",
-      scrollTrigger: {
-        trigger: ".project-container",
-        pin: true,
-        start: "top top",
-        scrub: 1,
-        snap: {
-          snapTo: 1 / (projects.length - 1),
-          duration: { min: 0.2, max: 0.3 },
-          delay: 0,
+      gsap.to(projects, {
+        xPercent: -100 * (projects.length - 1),
+        ease: "none",
+        scrollTrigger: {
+          trigger: ".project-container",
+          pin: true,
+          start: "top top",
+          scrub: 1,
+          snap: {
+            snapTo: 1 / (projects.length - 1),
+            duration: { min: 0.2, max: 0.3 },
+            delay: 0,
+          },
+          // Base vertical scrolling on how wide the container is so it feels more natural.
+          end: () => "+=" + document.querySelector(".project-row").offsetHeight,
         },
-        // Base vertical scrolling on how wide the container is so it feels more natural.
-        end: () => "+=" + document.querySelector(".project-row").offsetHeight,
-      },
-    });
+      });
+    };
+
+    homeAnimation(completeAnimation);
+
     return () => {
       window.removeEventListener("resize", debouncedHandleResize);
     };
@@ -135,84 +165,89 @@ export default function Home() {
         <title>Alexander Grattan Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <nav>
-        <div className="space-between">
-          <div className="logo">AG</div>
-          <ul className="nav-list">
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-            <li>
-              <Link href="/about">About Me</Link>
-            </li>
-            <li>
-              <Link href="/contact">Contact</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-      <main className="main">
-        <h1 className="title">
-          I create <span className="playful">playful</span> experiences.
-        </h1>
-        <h3 className="job-title">Alexander Grattan / Full Stack Developer</h3>
-        <div className="scroll-indicator">
-          <p>Projects</p>
-          <FontAwesomeIcon icon={faChevronDown} />
-        </div>
-      </main>
-      <div className="project-container">
-        <div className="project-row">
-          {projectArray.map(({ name, description, image, link, page }) => (
-            <div className="project">
-              <img src={image} alt={name} />
-              <div className="project-info">
-                <h3>{name}</h3>
-                <p>{description}</p>
-                <div className="project-btns">
-                  <a href={page} target="_blank" rel="noopener noreferrer">
-                    <button>Learn More</button>
-                  </a>
-                  <a href={link} target="_blank" rel="noopener noreferrer">
-                    <button>To Site</button>
-                  </a>
+      {animationComplete === false && <IntroOverlay />}
+      <div className="after-animation">
+        <nav>
+          <div className="space-between">
+            <div className="logo">AG</div>
+            <ul className="nav-list">
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/about">About Me</Link>
+              </li>
+              <li>
+                <Link href="/contact">Contact</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <main className="main">
+          <h1 className="title">
+            I create <span className="playful">playful</span> experiences.
+          </h1>
+          <h3 className="job-title">
+            Alexander Grattan / Full Stack Developer
+          </h3>
+          <div className="scroll-indicator" onClick={executeScroll}>
+            <p>Projects</p>
+            <FontAwesomeIcon icon={faChevronDown} />
+          </div>
+        </main>
+        <div className="project-container" ref={projectsRef}>
+          <div className="project-row">
+            {projectArray.map(({ name, description, image, link, page }) => (
+              <div className="project">
+                <img src={image} alt={name} />
+                <div className="project-info">
+                  <h3>{name}</h3>
+                  <p>{description}</p>
+                  <div className="project-btns">
+                    <a href={page} target="_blank" rel="noopener noreferrer">
+                      <button>Learn More</button>
+                    </a>
+                    <a href={link} target="_blank" rel="noopener noreferrer">
+                      <button>To Site</button>
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
+        <footer>
+          <ul className="footer-links">
+            <li>
+              <a
+                href="https://github.com/GameDog9988"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a
+                href="Resume_Alexander_Grattan.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Resume
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://twitter.com/agrattan0820"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Twitter
+              </a>
+            </li>
+          </ul>
+        </footer>
       </div>
-      <footer>
-        <ul className="footer-links">
-          <li>
-            <a
-              href="https://github.com/GameDog9988"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              GitHub
-            </a>
-          </li>
-          <li>
-            <a
-              href="Resume_Alexander_Grattan.pdf"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Resume
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://twitter.com/agrattan0820"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Twitter
-            </a>
-          </li>
-        </ul>
-      </footer>
     </div>
   );
 }
