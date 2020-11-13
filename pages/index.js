e2640cf8191a003adfab8fd809109248b2781787
Alexander Grattan <alex@grattan.me>
2020-11-12 19:49:25 -0500

    Responsive viewport height

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Link from "next/link";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const projectArray = [
@@ -40,9 +40,37 @@ export default function Home() {
     },
   ];
 
+  const [dimensions, setDimensions] = useState();
+
   useEffect(() => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    setDimensions({
+      height: window.innerHeight,
+      width: window.innerWidth,
+    });
+
+    function debounce(fn, ms) {
+      let timer;
+      return () => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+          timer = null;
+          fn.apply(this, arguments);
+        }, ms);
+      };
+    }
+
+    const debouncedHandleResize = debounce(function handleResize() {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth,
+      });
+      console.log(dimensions);
+    }, 1000);
+
+    window.addEventListener("resize", debouncedHandleResize);
 
     gsap.registerPlugin(ScrollTrigger);
 
@@ -68,6 +96,9 @@ export default function Home() {
 
     gsap.to("body", { duration: 0, css: { visibility: "visible" } });
     gsap.from(".title", { duration: 1, y: 20, opacity: 0, ease: "power4.out" });
+    return () => {
+      window.removeEventListener("resize", debouncedHandleResize);
+    };
   }, []);
 
   return (
