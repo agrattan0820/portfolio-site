1f0de404969e95e7bc0578fdc6ff538cf254022a
Alexander Grattan <alex@grattan.me>
2021-01-13 10:39:36 -0500

    feat: scroll to top on route change

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import "../styles/App.scss";
+import { useEffect } from "react";
 import { AnimatePresence } from "framer-motion";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
+  const { pathname } = useRouter();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <AnimatePresence exitBeforeEnter>
       <Component {...pageProps} />
