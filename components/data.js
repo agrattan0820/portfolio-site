3ceb6176babc1bea7ae99219f1aeea388f025614
Alexander Grattan <alex@grattan.me>
2021-02-26 10:14:11 -0500

    feat: pitt-csc and green pages

--- components/data.js
+++ components/data.js
@@ -1,6 +1,24 @@
 export const projectsList = [
   {
-    name: "grademyaid",
+    name: "Pitt CSC",
+    description:
+      "Appointed as a club officer for the Pitt Computer Science Club, both to support initiatives and to redesign the club website. The current website can befound here: https://pittcsc.org/. Designed iteratively with Figma and developed with Gatsby, Tailwind CSS, and Framer Motion.",
+    image: "/images/Pitt_CSC_Screenshot.png",
+    link: "https://pittcsc-crashtestdummy.netlify.app",
+    page: "",
+    tools: ["Gatsby", "Tailwind CSS", "Sass", "Framer Motion"],
+  },
+  {
+    name: "Green Pages",
+    description:
+      "Cooperated with another developer and two pharmacy students to create a website that tracks cannabis strain availability in the Pennsylvania area.Designed in Figma and developed the frontend with React and SCSS. Helped create the REST API and backend with Django Rest Framework.",
+    image: "/images/Green_Pages_Screenshot.png",
+    link: "https://green-pages-infotender.herokuapp.com/",
+    page: "",
+    tools: ["React", "Django", "Sass", "Redux", "Framer Motion"],
+  },
+  {
+    name: "Grademyaid",
     description:
       "A website that allows students to grade their college financial aid packages using data from the US Department of Education.\nThis was built for Pitt CSC Hacks, a competition sponsored by the University of Pittsburgh's Computer Science Club.",
     image: "/images/www.grademyaid.com_.png",
@@ -26,22 +44,22 @@ export const projectsList = [
     page: "https://github.com/GameDog9988/Virtual-Safari",
     tools: ["HTML", "CSS", "JavaScript"],
   },
-  {
-    name: "Danny's Subs",
-    description:
-      "This ficticious restaurant website was my first foray into learning React and using the GreenSock Animation Platform.\nThe photos came from Unsplash, a site that I love using to find high quality photos.",
-    image: "/images/Danny_Subs.png",
-    link: "https://dannysubs.netlify.app",
-    page: "https://github.com/GameDog9988/Restaurant-React",
-    tools: ["React", "GSAP"],
-  },
-  {
-    name: "Tiny Jim's Smokehouse",
-    description:
-      "One of my first multi-page fully responsive websites for a ficticious restaurant.\nUses the Google Maps API to show the restaurant location.",
-    image: "/images/Tiny_Jims_Smokehouse.png",
-    link: "https://tinyjimsbbq.netlify.app",
-    page: "https://github.com/GameDog9988/Tiny-Jims-Smokehouse",
-    tools: ["HTML", "CSS", "JavaScript", "Google Maps API"],
-  },
+  // {
+  //   name: "Danny's Subs",
+  //   description:
+  //     "This ficticious restaurant website was my first foray into learning React and using the GreenSock Animation Platform.\nThe photos came from Unsplash, a site that I love using to find high quality photos.",
+  //   image: "/images/Danny_Subs.png",
+  //   link: "https://dannysubs.netlify.app",
+  //   page: "https://github.com/GameDog9988/Restaurant-React",
+  //   tools: ["React", "GSAP"],
+  // },
+  // {
+  //   name: "Tiny Jim's Smokehouse",
+  //   description:
+  //     "One of my first multi-page fully responsive websites for a ficticious restaurant.\nUses the Google Maps API to show the restaurant location.",
+  //   image: "/images/Tiny_Jims_Smokehouse.png",
+  //   link: "https://tinyjimsbbq.netlify.app",
+  //   page: "https://github.com/GameDog9988/Tiny-Jims-Smokehouse",
+  //   tools: ["HTML", "CSS", "JavaScript", "Google Maps API"],
+  // },
 ];
