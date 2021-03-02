5558e2f20be0635fa1fe7503a407077b1f06de03
Alexander Grattan <alex@grattan.me>
2021-03-02 18:14:49 -0500

    feat: add hotjar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import "../styles/App.scss";
 import { AnimatePresence } from "framer-motion";
+import { hotjar } from "react-hotjar";
+import ReactGA from "react-ga";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // Google Analytics
+    ReactGA.initialize("UA-183066430-1");
+    ReactGA.pageview("/");
+
+    // Hotjar
+    hotjar.initialize(2276411, 6);
+  }, []);
+
   return (
     <AnimatePresence exitBeforeEnter>
       <Component {...pageProps} />
