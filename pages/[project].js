16ddfb2dd23a1b359cb664a68bd68f7151ee06da
GameDog9988 <alex@grattan.me>
2022-01-04 19:09:42 -0500

    feat: place nav in header

--- pages/[project].js
+++ pages/[project].js
@@ -37,53 +37,55 @@ export default function Project({ projectObject }) {
         <title>{projectObject?.name} | Alexander Grattan</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <nav className="project-nav">
-        <div className="space-between">
-          <Link href="/">
-            <div className="logo">AG</div>
-          </Link>
-          <ul className="nav-list">
-            <li>
-              <motion.a
-                href="https://drive.google.com/file/d/1PgvpHThs5XjTwGZgib9ZTVLa8QbhulWp/view?usp=sharing"
-                target="_blank"
-                rel="noopener noreferrer"
-                whileHover={{ scale: 1.05 }}
-                whileTap={{ scale: 0.95 }}
-                title="Download Alexander's Resume"
-              >
-                Resume
-              </motion.a>
-            </li>
-            <li>
-              <motion.a
-                href="https://github.com/GameDog9988"
-                target="_blank"
-                rel="noopener noreferrer"
-                whileHover={{ scale: 1.05 }}
-                whileTap={{ scale: 0.95 }}
-                title="Go to Alexander's GitHub"
-              >
-                <FontAwesomeIcon icon={faGithub} size="2x" />
-                <span className="header-hidden-text">GitHub</span>
-              </motion.a>
-            </li>
-            <li>
-              <motion.a
-                href="https://www.linkedin.com/in/alexander-grattan/"
-                target="_blank"
-                rel="noopener noreferrer"
-                whileHover={{ scale: 1.05 }}
-                whileTap={{ scale: 0.95 }}
-                title="Connect with Alexander on LinkedIn"
-              >
-                <FontAwesomeIcon icon={faLinkedin} size="2x" />
-                <span className="header-hidden-text">LinkedIn</span>
-              </motion.a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <header>
+        <nav className="project-nav">
+          <div className="space-between">
+            <Link href="/">
+              <div className="logo">AG</div>
+            </Link>
+            <ul className="nav-list">
+              <li>
+                <motion.a
+                  href="https://drive.google.com/file/d/1PgvpHThs5XjTwGZgib9ZTVLa8QbhulWp/view?usp=sharing"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  whileHover={{ scale: 1.05 }}
+                  whileTap={{ scale: 0.95 }}
+                  title="Download Alexander's Resume"
+                >
+                  Resume
+                </motion.a>
+              </li>
+              <li>
+                <motion.a
+                  href="https://github.com/GameDog9988"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  whileHover={{ scale: 1.05 }}
+                  whileTap={{ scale: 0.95 }}
+                  title="Go to Alexander's GitHub"
+                >
+                  <FontAwesomeIcon icon={faGithub} size="2x" />
+                  <span className="header-hidden-text">GitHub</span>
+                </motion.a>
+              </li>
+              <li>
+                <motion.a
+                  href="https://www.linkedin.com/in/alexander-grattan/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  whileHover={{ scale: 1.05 }}
+                  whileTap={{ scale: 0.95 }}
+                  title="Connect with Alexander on LinkedIn"
+                >
+                  <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                  <span className="header-hidden-text">LinkedIn</span>
+                </motion.a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
       <motion.main
         initial="hidden"
         animate={controls}
--- pages/index.js
+++ pages/index.js
@@ -184,54 +184,57 @@ export default function Home() {
       </Head>
       {animationComplete === false && <IntroOverlay />}
       <div className="after-animation">
-        <nav className="home-nav">
-          <div className="space-between">
-            <Link href="/">
-              <div className="logo">AG</div>
-            </Link>
+        <header>
+          <nav className="home-nav">
+            <div className="space-between">
+              <Link href="/">
+                <div className="logo">AG</div>
+              </Link>
 
-            <ul className="nav-list">
-              <li>
-                <motion.a
-                  href="https://drive.google.com/file/d/1PgvpHThs5XjTwGZgib9ZTVLa8QbhulWp/view?usp=sharing"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  whileHover={{ scale: 1.1 }}
-                  whileTap={{ scale: 0.9 }}
-                  title="Download Alexander's Resume"
-                >
-                  Resume
-                </motion.a>
-              </li>
-              <li>
-                <motion.a
-                  href="https://github.com/GameDog9988"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  whileHover={{ scale: 1.05 }}
-                  whileTap={{ scale: 0.95 }}
-                  title="Go to Alexander's GitHub"
-                >
-                  <FontAwesomeIcon icon={faGithub} size="2x" />
-                  <span className="header-hidden-text">GitHub</span>
-                </motion.a>
-              </li>
-              <li>
-                <motion.a
-                  href="https://www.linkedin.com/in/alexander-grattan/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  whileHover={{ scale: 1.05 }}
-                  whileTap={{ scale: 0.95 }}
-                  title="Connect with Alexander on LinkedIn"
-                >
-                  <FontAwesomeIcon icon={faLinkedin} size="2x" />
-                  <span className="header-hidden-text">LinkedIn</span>
-                </motion.a>
-              </li>
-            </ul>
-          </div>
-        </nav>
+              <ul className="nav-list">
+                <li>
+                  <motion.a
+                    href="https://drive.google.com/file/d/1PgvpHThs5XjTwGZgib9ZTVLa8QbhulWp/view?usp=sharing"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                    title="Download Alexander's Resume"
+                  >
+                    Resume
+                  </motion.a>
+                </li>
+                <li>
+                  <motion.a
+                    href="https://github.com/GameDog9988"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.95 }}
+                    title="Go to Alexander's GitHub"
+                  >
+                    <FontAwesomeIcon icon={faGithub} size="2x" />
+                    <span className="header-hidden-text">GitHub</span>
+                  </motion.a>
+                </li>
+                <li>
+                  <motion.a
+                    href="https://www.linkedin.com/in/alexander-grattan/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.95 }}
+                    title="Connect with Alexander on LinkedIn"
+                  >
+                    <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                    <span className="header-hidden-text">LinkedIn</span>
+                  </motion.a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </header>
+
         <main className="main-home">
           <div className="cta">
             <h1 className="title">
