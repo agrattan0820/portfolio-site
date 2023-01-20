cdfbe67daeb819c1691052315493b09e972b28e7
GameDog9988 <alex@grattan.me>
2023-01-19 20:07:33 -0500

    feat: register google analytics page views for each page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,24 @@
 import "../styles/App.scss";
 import { useEffect } from "react";
 import { AppProps } from 'next/app'
+import { useRouter } from "next/router";
 import { AnimatePresence } from "framer-motion";
 import { hotjar } from "react-hotjar";
 import ReactGA from "react-ga";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   useEffect(() => {
     // Google Analytics
     ReactGA.initialize("UA-183066430-1");
-    ReactGA.pageview("/");
-
     // Hotjar
     hotjar.initialize(2276411, 6);
   }, []);
+
+  useEffect(() => {
+    ReactGA.pageview(router.asPath);
+  }, [router.asPath])
 
   return (
     <AnimatePresence exitBeforeEnter>
