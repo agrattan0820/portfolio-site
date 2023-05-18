73d5d3e94036c2eaf7936ca96726dcc5683ee287
GameDog9988 <alex@grattan.me>
2023-05-17 21:32:34 -0400

    feat: initial post functionality and styling

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -8,16 +8,40 @@ import html from "remark-html";
 import matter from "gray-matter";
 import fs from "fs";
 import path from "path";
+import { motion } from "framer-motion";
+import SEO from "../../components/seo";
+import Header from "../../components/header";
 
 const postsDirectory = path.join(process.cwd(), "posts");
 
-export default function Post(
-  props: InferGetStaticPropsType<typeof getStaticProps>
-) {
+type PostPageProps = {
+  title: string;
+  date: string;
+  slug: string;
+  contentHtml: string;
+};
+
+export default function Post(props: PostPageProps) {
   return (
-    <div>
-      <div dangerouslySetInnerHTML={{ __html: props.contentHtml }} />
-    </div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      className="container"
+    >
+      <SEO
+        title={`${props?.title} | Alexander Grattan`}
+        url={`https://agrattan.com/blog/${props.slug}`}
+      />
+      <Header logoLink={`/`} />
+      <div className="post-main">
+        <h1 className="post-title">{props.title}</h1>
+        <div className="post-date">
+          {new Date(props.date).toLocaleDateString()}
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: props.contentHtml }} />
+      </div>
+    </motion.div>
   );
 }
 
@@ -41,7 +65,6 @@ export const getStaticPaths = async (context: GetStaticPathsContext) => {
 export const getStaticProps = async (context: GetStaticPropsContext) => {
   const slug = context.params.slug;
   const fullPath = path.join(postsDirectory, `${slug}.md`);
-  console.log(fullPath);
   const fileContents = fs.readFileSync(fullPath, "utf8");
 
   if (!fileContents) {
@@ -62,7 +85,7 @@ export const getStaticProps = async (context: GetStaticPropsContext) => {
     props: {
       slug,
       contentHtml,
-      ...matterResult.data,
+      ...JSON.parse(JSON.stringify(matterResult.data)),
     },
   };
 };
