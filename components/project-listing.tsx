33c5b16810c7bd3f213afca65567e50fe0a93287
GameDog9988 <alex@grattan.me>
2023-05-20 10:11:36 -0400

    feat: styling improvements to the project page

--- components/project-listing.tsx
+++ components/project-listing.tsx
@@ -24,65 +24,67 @@ const ProjectListing = ({ project }: ProjectListingProps) => {
 
   return (
     <div className="project" id={slug}>
-      <Link href={slug}>
-        <picture>
-          {mobileImage && (
-            <source srcSet={mobileImage.src} media="(max-width:967px)" />
-          )}
-          <img src={image.src} alt={name} className="project-image" />
-        </picture>
-      </Link>
-      <div className="project-info">
+      <div className="project-container">
         <Link href={slug}>
-          <h2>{name}</h2>
+          <picture>
+            {mobileImage && (
+              <source srcSet={mobileImage.src} media="(max-width:967px)" />
+            )}
+            <img src={image.src} alt={name} className="project-image" />
+          </picture>
         </Link>
-        {description.split("\n").map((str, index) => (
-          <p key={index}>{str}</p>
-        ))}
-        {longDescription && (
+        <div className="project-info">
           <Link href={slug}>
-            <button className="project-read-more">
-              <span>Read More</span>{" "}
-              <div className="read-more-arrow">
-                <FaChevronRight />
-              </div>
-            </button>
+            <h2>{name}</h2>
           </Link>
-        )}
-        <h3>Tools used:</h3>
-        <ul className="tools-list">
-          {tools.map((item, index) => (
-            <li key={index}>{item}</li>
+          {description.split("\n").map((str, index) => (
+            <p key={index}>{str}</p>
           ))}
-        </ul>
-        <motion.div className="project-btns">
-          {link && (
-            <motion.a
-              href={link}
-              target="_blank"
-              rel="noopener noreferrer"
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.95 }}
-              title={`Open site of ${name}`}
-              className="project-btn"
-            >
-              Open Site
-            </motion.a>
+          {longDescription && (
+            <Link href={slug}>
+              <button className="project-read-more">
+                <span>Read More</span>{" "}
+                <div className="read-more-arrow">
+                  <FaChevronRight />
+                </div>
+              </button>
+            </Link>
           )}
-          {code && (
-            <motion.a
-              href={code}
-              target="_blank"
-              rel="noopener noreferrer"
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.95 }}
-              title={`View Code for ${name}`}
-              className="project-btn"
-            >
-              View Code
-            </motion.a>
-          )}
-        </motion.div>
+          <h3>Tools used:</h3>
+          <ul className="tools-list">
+            {tools.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+          <motion.div className="project-btns">
+            {link && (
+              <motion.a
+                href={link}
+                target="_blank"
+                rel="noopener noreferrer"
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+                title={`Open site of ${name}`}
+                className="project-btn"
+              >
+                Open Site
+              </motion.a>
+            )}
+            {code && (
+              <motion.a
+                href={code}
+                target="_blank"
+                rel="noopener noreferrer"
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+                title={`View Code for ${name}`}
+                className="project-btn"
+              >
+                View Code
+              </motion.a>
+            )}
+          </motion.div>
+        </div>
       </div>
     </div>
   );
