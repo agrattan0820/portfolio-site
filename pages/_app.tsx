e4517bf215656789666462d313bcdf3845a8a310
GameDog9988 <alex@grattan.me>
2023-12-11 21:32:46 -0500

    feat: use next/font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,20 @@ import "../styles/app.scss";
 import { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
 import { AnimatePresence } from "framer-motion";
+import { Raleway } from "next/font/google";
+
+const raleway = Raleway({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-raleway",
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AnimatePresence mode="wait">
-      <Component {...pageProps} />
+      <div className={raleway.className}>
+        <Component {...pageProps} />
+      </div>
       <Analytics />
     </AnimatePresence>
   );
