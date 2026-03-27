8f251d6628809fe5922835b0b9c262c55026d772
Alexander Grattan <alex@grattan.me>
2026-03-27 12:07:35 -0400

    fix: working build

---
+++ app/blog/post/falling-out-of-love-with-twin-macro/page.tsx
@@ -0,0 +1,7 @@
+import Content, { metadata } from "./content.mdx";
+
+export { metadata };
+
+export default function Page() {
+  return <Content />;
+}
---
+++ mdx-custom.d.ts
@@ -0,0 +1,5 @@
+declare module "*.mdx" {
+  import type { Metadata } from "next";
+
+  export const metadata: Metadata;
+}
